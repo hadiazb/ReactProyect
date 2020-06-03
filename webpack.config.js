89276fedf8e84f5df4dd06e8d36ddf89de7e36df
hugo <hugoandresdiazbernal@gmail.com>
2020-06-02 19:37:38 -0500

    Añadiendo lista de items y otras cosas

--- webpack.config.js
+++ webpack.config.js
@@ -29,16 +29,26 @@ module.exports = {
       {
         test: /\.(s*)css$/,
         use: [
-          { 
-            loader: MiniCssExtractPlugin.loader 
+          {
+            loader: MiniCssExtractPlugin.loader,
           },
           'css-loader',
           'sass-loader',
         ],
-      }, 
+      },
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/[hash].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
-
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
@@ -48,4 +58,4 @@ module.exports = {
       filename: 'assets/[name].css',
     }),
   ],
-};
+}
